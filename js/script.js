e04dfc45f8092a8ec9eec892a6a69f51f38dbb47
Jubril Abdulsalam <abdulsalamjubr3@gmail.com>
2023-11-21 13:10:19 +0100

    fixed responsiveness issue on home page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
+// Nav bar
+const navButton = document.querySelector('.mobile-nav');
+const navLinks = document.querySelector('.primary-navigation');
+
+navButton.addEventListener('click', () => {
+    let state = navButton.getAttribute('data-state');
+
+    if (state === "collapsed") {
+        navButton.setAttribute("data-state", "expanded");
+        navButton.children[0].classList.remove('fa-bars')
+        navButton.children[0].classList.add('fa-xmark')
+        navLinks.classList.add("visible");
+        
+    } else {
+        navButton.setAttribute("data-state", "collapsed");
+        navButton.children[0].classList.remove('fa-xmark')
+        navButton.children[0].classList.add('fa-bars')
+        navLinks.classList.remove("visible");
+    }
+})
+
+// Hero Section
 const hero = document.querySelector('.hero');
 const bgNavItems = document.querySelectorAll('.bg-nav-item');
 
 bgNavItems.forEach((item) => {
     item.addEventListener('click', () => {
         hero.style.backgroundImage = `url('assets/images/hero-${item.id}.jpg')`;
+        hero.style.backgroundPosition = 'center';
     })
 })
