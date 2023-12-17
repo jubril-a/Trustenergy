27aa8f22b01576b99d5730a5313f7ee5a43dc18d
Newtayo <akandeabdul@gmail.com>
2023-12-17 18:55:20 +0100

    Fix deployment issues

--- src/career.js
+++ src/career.js
@@ -14,65 +14,89 @@ document.addEventListener('DOMContentLoaded', function(){
     footer()
 })
 
-document.getElementById('sendEmail').addEventListener('click', async () => {
+document.getElementById('careerForm').addEventListener('submit', async function submitForm(event) {
+    event.preventDefault();
+     // Prevent the default form submission
     console.log("click")
+    const form = document.getElementById('careerForm');
+    const formData = new FormData(form);
+
     try {
-      const response = await fetch('http://localhost:3001/send-email', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        // Additional request options
-      });
-  
-      if (response.ok) {
-        const result = await response.text();
-        console.log(result);
-      } else {
-        console.error('Failed to send email');
-      }
+        const response = await fetch('https://trustenergy.onrender.com/api/upload', {
+            method: 'POST',
+            body: formData,
+        });
+
+        if (response.ok) {
+            console.log('File uploaded successfully');
+            // Optionally, perform additional actions on success
+        } else {
+            console.error('File upload failed');
+        }
     } catch (error) {
-      console.error(error);
+        console.error('An error occurred:', error);
     }
-  });
+})
+
+// document.getElementById('sendEmail').addEventListener('click', async () => {
+//     console.log("click")
+//     try {
+//       const response = await fetch('http://localhost:3001/send-email', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         // Additional request options
+//       });
+  
+//       if (response.ok) {
+//         const result = await response.text();
+//         console.log(result);
+//       } else {
+//         console.error('Failed to send email');
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   });
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBCmEKdmXuqNxrVuQbD_n1ErqNXu2TpjUk",
-  authDomain: "trustenergybackend.firebaseapp.com",
-  projectId: "trustenergybackend",
-  storageBucket: "trustenergybackend.appspot.com",
-  messagingSenderId: "409188412513",
-  appId: "1:409188412513:web:2ccc6584f5f43c5ac1ce20",
-  measurementId: "G-M1K3N92ET8"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBCmEKdmXuqNxrVuQbD_n1ErqNXu2TpjUk",
+//   authDomain: "trustenergybackend.firebaseapp.com",
+//   projectId: "trustenergybackend",
+//   storageBucket: "trustenergybackend.appspot.com",
+//   messagingSenderId: "409188412513",
+//   appId: "1:409188412513:web:2ccc6584f5f43c5ac1ce20",
+//   measurementId: "G-M1K3N92ET8"
+// };
 
 // // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
-// const storageRef = ref(storage);
+// const app = initializeApp(firebaseConfig);
+// const storage = getStorage(app);
+// // const storageRef = ref(storage);
 
-// const cvRef = ref(storage, 'cv')
+// // const cvRef = ref(storage, 'cv')
 
-// uploadBytes(cvRef, file).then((snapshot) => {
-//     console.log("Uploaded a file")
+// // uploadBytes(cvRef, file).then((snapshot) => {
+// //     console.log("Uploaded a file")
+// // })
+
+// const submitForm =()=>{
+//     const cvUpload = document.getElementById('cv')
+//     const role = document.getElementById('jobrole')
+//     console.log(cvUpload.files[0])
+//     console.log(role.value)
+//     const storage = getStorage();
+//     const storageRef = ref(storage, `${role.value}CV`);
+//     uploadBytes(storageRef, cvUpload.files[0]).then((snapshot) => {
+//         console.log('Uploaded a blob or file!');
+//       });
+// }
+
+// const upload = document.getElementById('upload')
+
+// upload.addEventListener('click', (e)=>{
+//    e.preventDefault()
+//    submitForm()
 // })
-
-const submitForm =()=>{
-    const cvUpload = document.getElementById('cv')
-    const role = document.getElementById('jobrole')
-    console.log(cvUpload.files[0])
-    console.log(role.value)
-    const storage = getStorage();
-    const storageRef = ref(storage, `${role.value}CV`);
-    uploadBytes(storageRef, cvUpload.files[0]).then((snapshot) => {
-        console.log('Uploaded a blob or file!');
-      });
-}
-
-const upload = document.getElementById('upload')
-
-upload.addEventListener('click', (e)=>{
-   e.preventDefault()
-   submitForm()
-})
