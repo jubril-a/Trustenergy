38e98ce0fa8ccfe042732e76b9b502f1b9c8dcdb
Newtayo <akandeabdul@gmail.com>
2023-12-17 17:53:17 +0100

    Fix deployment issues

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
-// server.js
 const express = require('express');
 const nodemailer = require('nodemailer');
 const multer = require('multer');
 const path = require('path');
 const app = express();
-const cors = require('cors')
-app.use(cors())
+const cors = require('cors');
+app.use(cors());
 const port = process.env.PORT || 3003;
 
 const storage = multer.memoryStorage();
@@ -14,44 +13,51 @@ const upload = multer({ storage: storage });
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.post('/api/upload', upload.single('cv'), (req, res) => {
+  try {
     const { role } = req.body;
     const attachment = req.file.buffer;
 
+    if (!role || !attachment) {
+      return res.status(400).send('Bad Request: Role and CV are required.');
+    }
+
     const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true, // use SSL
-        auth: {
-          user: "akandeabdul@gmail.com",
-          pass: "sand ygai lgzb spwy",
-        },
-      });
-      const mailOptions = {
-        from: 'akandeabdul@gmail.com',
-        to: 'akandeabdul@gmail.com',
-        subject: `Application for the role of ${role}`,
-        attachments: [
-          {
-            filename: 'cv.pdf', // Change the filename as needed
-            content: attachment.toString('base64'), // Convert buffer to base64
-            encoding: 'base64', // Set encoding to base64
-          },
-        ],
-      };
-    
-      // Send email
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          return res.status(500).send(error.toString());
-        }
-        res.send(`Email sent: ${info.response}`);
-      });
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // use SSL
+      auth: {
+        user: 'akandeabdul@gmail.com',
+        pass: 'sand ygai lgzb spwy',
+      },
     });
 
+    const mailOptions = {
+      from: 'akandeabdul@gmail.com',
+      to: 'akandeabdul@gmail.com',
+      subject: `Application for the role of ${role}`,
+      attachments: [
+        {
+          filename: 'cv.pdf',
+          content: attachment.toString('base64'),
+          encoding: 'base64',
+        },
+      ],
+    };
 
-
-
-  
+    // Send email
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+        return res.status(500).send(error.toString());
+      }
+      console.log('Email sent:', info.response);
+      res.send(`Email sent: ${info.response}`);
+    });
+  } catch (err) {
+    console.error('Unexpected error:', err);
+    res.status(500).send(err.toString());
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
